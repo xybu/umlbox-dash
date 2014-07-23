fe22aecf38d4c8fd95fab0c62ccf3738b391dbbd
Xiangyu Bu <xybu92@live.com>
2014-07-22 21:59:14 -0400

    Update test.sh

--- example/test.sh
+++ example/test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# /usr/bin/umlbox -B -fw ./rw -f . -m 16m -T 20 `pwd`/../limits /usr/bin/nice -n10 ./sample.sh
-/usr/bin/umlbox -B -fw ./rw -f . -m 48m -T 20 `pwd`/ulimits /usr/bin/nice -n10 time ./sample.sh
+# Run sample.sh (containing I/O operations) in sandbox.
+# Xiangyu Bu <xybu92@live.com>
+
+# ../ulimits sets more restrictions on the process via `ulimit`
+# use `nice` to change process priority
+# . is mounted as R-only
+# ./rw is mounted RW-only
+# memory ceiling is set to 48MiB
+# kill the process after 20 seconds
+/usr/bin/umlbox -B -fw ./rw -f . -m 48m -T 20 `pwd`/ulimits /usr/bin/nice -n10 ./sample.sh
